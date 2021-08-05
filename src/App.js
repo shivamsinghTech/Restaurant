fe63fbefdca8c43dcb24fbe788aeb2438a74ba71
shivamsinghrajputhakur <shivamkondersingh@gmail.com>
2021-08-05 03:54:06 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+import Restraunt from "./Components/Basics/Restraunt"
+
+
+
+function App() {
+  return (
+    <>
+  
+
+        <Restraunt/>
+        
+      
+    </>
+  );
+}
+
+export default App;
